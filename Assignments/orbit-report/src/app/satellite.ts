f785a3d4f8def2a7912a818c00cedde45be7649d
Anusha <anushabt@gmail.com>
2020-12-09 22:01:19 -0600

    Highlight Space Debris

--- Assignments/orbit-report/src/app/satellite.ts
+++ Assignments/orbit-report/src/app/satellite.ts
@@ -11,4 +11,10 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+    shouldShowWarning():boolean {
+        if(this.type.toLowerCase() === 'space debris'){
+            return true;
+        }
+        return false;
+    }
 }
